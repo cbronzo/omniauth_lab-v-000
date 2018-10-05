97c54e2d43f84557444670db99abff5056e02b0b
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:44:24 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  
+  def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+    end
+     login!
+    render 'welcome/home'
+  end
+   def destroy
+    session.clear
+  end
+   def auth
+    request.env['omniauth.auth']
+  end
+  
+  
 end
