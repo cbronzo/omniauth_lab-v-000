84dc193a5879c1b2e483e51b332c2c03baa64446
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:50:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 class SessionsController < ApplicationController
   
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
+    user = User.find_or_create_by(:uid => auth['uid']) do |user|
+      user.name = auth['info']['name']
     end
-     login!
-    render 'welcome/home'
+    session[:user_id] = user.try(:id)
   end
   
   def destroy
